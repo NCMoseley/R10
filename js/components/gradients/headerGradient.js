56d2d2f6087af4f1031b5ee76c19232c382c2269
NCMoseley <ncmoseley@gmail.com>
2018-03-05 23:06:57 -0800

    Add styles to linear Gradient

--- js/components/gradients/headerGradient.js
+++ js/components/gradients/headerGradient.js
@@ -1,11 +1,13 @@
 import React from "react";
 import LinearGradient from "react-native-linear-gradient";
 
+// This is Lindseys Gradient
+
 const HeaderGradient = () => (
   <LinearGradient
-    start={{ x: 0.0, y: 0.25 }}
-    end={{ x: 0.25, y: 1.0 }}
-    colors={["red", "purple"]}
+    start={{ y: 0.0, x: 1.0 }}
+    end={{ y: 1.0, x: 0.0 }}
+    colors={["#9963ea", "#cf392a"]}
     style={{ flex: 1 }}
   />
 );
